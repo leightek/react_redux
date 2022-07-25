095764d13389d0e83749a5eec80cfa77943b66a1
leightek <leighsuconsulting@gmail.com>
2022-07-24 23:33:24 -0700

    [Redux] Updated button functions using Redux Toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,43 @@
-import {createStore} from 'redux';
+import {configureStore, createSlice} from '@reduxjs/toolkit';
 
-const reducerFn = (state = {counter: 0}, action) => {
-    // Synchronous Function
-    // should not mutate the original state
-    if (action.type === "INC") {
-        return {counter: state.counter + 1};   // ++ not working
+const counterSlice = createSlice({
+    name: "counter",
+    initialState: {counter: 0},
+    reducers: {
+        increment(state, action) {
+            state.counter++;  // not allowed hard-coded value
+        },
+        decrement(state, action) {
+            state.counter--;
+        },
+        addBy(state, action) {
+            state.counter += action.payload;
+        }
+
     }
 
-    if (action.type === "DEC") {
-        return {counter: state.counter - 1};
-    }
+})
 
-    if (action.type === "ADD") {
-        return {counter: state.counter + action.payload}
-    }
+// const reducerFn = (state = {counter: 0}, action) => {
+//     // Synchronous Function
+//     // should not mutate the original state
+//     if (action.type === "INC") {
+//         return {counter: state.counter + 1};   // ++ not working
+//     }
 
-    return state;
-}
+//     if (action.type === "DEC") {
+//         return {counter: state.counter - 1};
+//     }
 
-const store = createStore(reducerFn);
+//     if (action.type === "ADD") {
+//         return {counter: state.counter + action.payload}
+//     }
+
+//     return state;
+// }
+
+export const actions = counterSlice.actions;
+const store = configureStore({
+    reducer: counterSlice.reducer
+});
 export default store;
